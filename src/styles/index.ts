7763f752d17c3b57bf2b6222fd83263e2bf19186
jyh071116 <jyh071116@gmail.com>
2024-02-23 08:39:58 +0900

    chore: index 파일 생성

---
+++ src/styles/index.ts
@@ -0,0 +1,4 @@
+export { default as theme } from "./theme";
+export { default as font } from "./font";
+export { default as flex } from "./flex";
+export { default as GlobalStyle } from "./globalStyle";
