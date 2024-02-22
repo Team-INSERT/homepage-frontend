ca1fea203406971bd6f24fbe67ebdf5832b39138
jyh071116 <jyh071116@gmail.com>
2024-02-23 08:39:39 +0900

    style: Theme Color 지정

---
+++ src/styles/theme.ts
@@ -0,0 +1,22 @@
+const theme = {
+  black: "#000000",
+
+  grey100: "#F2F2F2",
+  grey200: "#D9D9D9",
+  grey300: "#CCCCCC",
+  grey400: "#B3B3B3",
+  grey500: "#999999",
+  grey600: "#808080",
+  grey700: "#666666",
+  grey800: "#4D4D4D",
+  grey900: "#333333",
+
+  bssmBlue: "007CFF",
+  white: "#007CFF",
+  pink: "#FF3BAB",
+  green: "#00D800",
+  red: "#FF0041",
+  yellow: "#D6FF00",
+};
+
+export default theme;
