6ee7b5a4ff82b4e490b279f084b510ba574aac75
jyh071116 <jyh071116@gmail.com>
2024-02-23 16:03:14 +0900

    feat(button): button 컴포넌트 작성 완료

---
+++ src/components/@common/Button.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import styled, { RuleSet, css } from "styled-components";
+import { theme } from "@/styles";
+
+type ButtonMode = "BLUE" | "NO_GLOW" | "GLOW" | "WHITE";
+type RadiusMode = number | "MAX";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  mode: ButtonMode;
+  radius: RadiusMode;
+}
+
+const ButtonType = (mode: ButtonMode): RuleSet<object> => {
+  switch (mode) {
+    case "BLUE":
+      return css`
+        background-color: ${theme.bssmBlue};
+        border: none;
+        color: ${theme.white};
+      `;
+    case "WHITE":
+      return css`
+        background-color: ${theme.grey100};
+        border: none;
+        color: ${theme.grey400};
+      `;
+    case "GLOW":
+      return css`
+        background-color: transparent;
+        border: 2px solid ${theme.bssmBlue};
+        color: ${theme.bssmBlue};
+      `;
+    case "NO_GLOW":
+      return css`
+        background-color: transparent;
+        border: 1.5px solid ${theme.grey400};
+        color: ${theme.black};
+      `;
+    default:
+      return css``;
+  }
+};
+
+const RadiusType = (radius: RadiusMode): RuleSet<object> => {
+  switch (radius) {
+    case "MAX":
+      return css`
+        border-radius: 999px;
+        padding: 8px 16px;
+      `;
+    default:
+      return css`
+        border-radius: ${radius}px;
+        padding: 12px 20px;
+      `;
+  }
+};
+
+const Button = ({ mode, radius, ...props }: ButtonProps) => {
+  return (
+    <StyledButton
+      mode={ButtonType(mode)}
+      radius={RadiusType(radius)}
+      {...props}
+    />
+  );
+};
+
+const StyledButton = styled.button<{
+  mode: RuleSet<object>;
+  radius: RuleSet<object>;
+}>`
+  width: fit-content;
+  ${({ mode }) => mode}
+  ${({ radius }) => radius}
+`;
+
+export default Button;
