d7a9a450e7848ed8c97786a0d8df89d669a479d7
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-02-24 22:25:59 +0900

    feat(footer) : 푸터 퍼블리싱 완료

---
+++ src/components/@common/Footer/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import BsmLogo from "@/assets/BsmLogo";
+import * as S from "./style";
+
+interface HomeProps {
+  color: string;
+}
+
+// eslint-disable-next-line react/prop-types
+const Home: React.FC<HomeProps> = ({ color }) => {
+  const { serviceInfo, policy, copyRight } = {
+    serviceInfo: "46708 부산 강서구 가락대로 1393 (봉림동 15)",
+    policy:
+      "교무실(입학처) : 051-971-2153, Fax : 051-971-2061    |    행정실 : 051-971-2152, Fax : 051-971-6325",
+    copyRight: "© 부산소프트웨어마이스터고등학교 All Rights Reserved.",
+  };
+
+  return (
+    <S.Container color={color}>
+      <BsmLogo />
+      <S.Info>
+        <div>{serviceInfo}</div>
+        <div>{policy}</div>
+      </S.Info>
+      <S.Line />
+      <S.CopyInfo>{copyRight}</S.CopyInfo>
+    </S.Container>
+  );
+};
+
+export default Home;
