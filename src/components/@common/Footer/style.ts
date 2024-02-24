194a7d699044ad5aa965ec13a9399c6eed5f90f0
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-02-24 22:30:03 +0900

    style(footer) : width 지정

--- src/components/@common/Footer/style.ts
+++ src/components/@common/Footer/style.ts
@@ -2,6 +2,7 @@ import { flex, font, theme } from "@/styles";
 import styled from "styled-components";
 
 export const Container = styled.div`
+  width: 100%;
   ${flex.FLEX}
   ${flex.COLUMN_FLEX}
   ${font.Caption}
