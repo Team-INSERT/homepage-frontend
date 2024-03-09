1c765339df360f33fb5a67c7293129a1f818888b
jyh071116 <jyh071116@gmail.com>
2024-03-10 00:36:24 +0900

    feat(auth): Auth 페이지 퍼블리싱

--- src/app/(admin)/auth/page.tsx
+++ src/app/(admin)/auth/page.tsx
@@ -1,3 +1,20 @@
+import GoogleIcon from "@/assets/GoogleIcon";
+import * as S from "./style";
+
 export default function Home() {
-  return <div>로그인</div>;
+  return (
+    <S.Background>
+      <S.Container>
+        <S.Text>
+          이곳은 BSSM 선생님 전용 페이지 입니다.
+          <br />
+          이용하시려면 학교 전용 구글 아이디로 로그인해주세요.
+        </S.Text>
+        <S.GoogleLoginBox>
+          <GoogleIcon />
+          로그인
+        </S.GoogleLoginBox>
+      </S.Container>
+    </S.Background>
+  );
 }
